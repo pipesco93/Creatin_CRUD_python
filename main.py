f5d990b8f2665e287b19bf0268caa3c56c74236e
Felipe Escobar R <pipesco93@gmail.com>
2022-01-15 18:40:56 -0500

    Primeras funciones para crear clientes en el programa

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+clients = 'pablo,ricardo,'
+
+
+def create_client(client_name):
+    global clients
+
+    clients += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clients
+    print(clients)
+
+
+def _add_comma():
+    global clients
+
+    clients += ','
+
+if __name__=='__main__':
+    list_clients()
+    create_client('Felipe')
+    list_clients()
